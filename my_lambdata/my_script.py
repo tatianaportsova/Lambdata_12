c358ec20e661543693bf4a3f008434b531b221e6
tatianaportsova <tt.sova@gmail.com>
2020-03-10 10:32:22 -0400

    Add a DataFrame to my_script.py

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,4 +1,5 @@
 import pandas
+import requests
 # my_lambdata/my_script.py
 
 from my_mod import enlarge
@@ -13,3 +14,7 @@ print("--------")
 x = 5
 print("NUMBER", x)
 print("ENLARGED NUMBER", enlarge(x))  # invoking our function!!
+
+df = pandas.read_csv('https://raw.githubusercontent.com/ryanleeallred/datasets/master/Ames%20Housing%20Data/train.csv')
+
+df['LotFrontage'].head(5)
