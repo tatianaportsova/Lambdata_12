ff9026bffe350ff505a58b708e9e08f90cb186a9
tatianaportsova <tt.sova@gmail.com>
2020-03-10 15:35:46 -0400

    Do train/val/test split, check for NaNs

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -18,3 +18,17 @@ print("ENLARGED NUMBER", enlarge(x))  # invoking our function!!
 df = pandas.read_csv('https://raw.githubusercontent.com/ryanleeallred/datasets/master/Ames%20Housing%20Data/train.csv')
 
 df['LotFrontage'].head(5)
+
+# Split train into train & val
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+train, test = train_test_split(df, train_size=0.85, test_size=0.15, 
+                               random_state=42)
+train, val = train_test_split(df, train_size=0.80, test_size=0.20, 
+                               random_state=42)
+
+train.shape, val.shape, test.shape
+
+# Check for NaN values in the dataset
+df.isna().sum() 
