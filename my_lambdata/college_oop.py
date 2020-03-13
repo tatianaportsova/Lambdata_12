4cab1dc4f65d479b8f2919155c4bb6b58243d8db
tatianaportsova <tt.sova@gmail.com>
2020-03-13 10:52:07 -0400

    PEP8 check college_oop.py & college_test.py

--- my_lambdata/college_oop.py
+++ my_lambdata/college_oop.py
@@ -1,6 +1,7 @@
 import pandas
 # colleges (OOP)
 
+
 class College():
 
     def __init__(self, name=None, city=None, state=None, funding=None):
@@ -8,51 +9,46 @@ class College():
         self.city = city
         self.state = state
         self.funding = funding
-    
-    
+
     def __str__(self):
         return f"<College {self.city} {self.name}>"
-    
+
     def __repr__(self):
         return f"<College {self.city} {self.name}>"
-   
+
     @property
     def full_info(self):
         return f"{self.city} {self.state} {self.funding} {self.name}"
-    
+
     def advertise(self):
         print(f"Welcome to {self.city.upper()}. Your revolution starts here.")
-   
+
     @staticmethod
     def advertise_generically():
         print("WELCOME!")
 
 if __name__ == "__main__":
-    college = College("Baruch College", "New York", "NY", "Public") 
+    college = College("Baruch College", "New York", "NY", "Public")
     print(college.full_info)
-    #college2 = College("Columbia University", "New York")
-    #print(college2.full_name)
-
 
     colleges_dict = [
         {"city": "New York", "state": "NY",
-        "funding": "Private, non-profit", "name": "Columbia University"},
+         "funding": "Private, non-profit", "name": "Columbia University"},
         {"city": "New York", "state": "NY",
-        "funding": "Private, non-profit", "name": "New York University"},
+         "funding": "Private, non-profit", "name": "New York University"},
         {"city": "New York", "state": "NY",
-        "funding": "Private, non-profit", "name": "Barnard College"},
+         "funding": "Private, non-profit", "name": "Barnard College"},
         {"city": "New York", "state": "NY",
-        "funding": "Public", "name": "Baruch College"},
+         "funding": "Public", "name": "Baruch College"},
         {"city": "New York", "state": "NY",
-        "funding": "Private, non-profit", "name": "Fordham University"}
+         "funding": "Private, non-profit", "name": "Fordham University"}
     ]
-    
+
     for college_d in colleges_dict:
-        college == College(college_d["name"],college_d["city"],
-                           college_d["state"],college_d["funding"])
+        college == College(college_d["name"], college_d["city"],
+                           college_d["state"], college_d["funding"])
         print(college.name)
         print(college.funding)
         print(college.full_info)
         print(college.advertise_generically())
         print("*----*----*")
-
--- test/college_test.py
+++ test/college_test.py
@@ -1,36 +1,37 @@
-
 # test.college_test.py
 
 import unittest
 from my_lambdata.college_oop import College
 
+
 class EDU(unittest.TestCase):
 
     def test_edu(self):
         college_dicts = [{"city": "New York", "state": "NY",
-        "funding": "Private, non-profit", "name": "Columbia University"},
-        {"city": "New York", "state": "NY",
-        "funding": "Private, non-profit", "name": "New York University"},
-        {"city": "New York", "state": "NY",
-        "funding": "Private, non-profit", "name": "Barnard College"},
-        {"city": "New York", "state": "NY",
-        "funding": "Public", "name": "Baruch College"},
-        {"city": "New York", "state": "NY",
-        "funding": "Private, non-profit", "name": "Fordham University"}]
-         # test to make sure this test was setup properly
+                          "funding": "Private, non-profit",
+                          "name": "Columbia University"},
+                         {"city": "New York", "state": "NY",
+                          "funding": "Private, non-profit",
+                          "name": "New York University"},
+                         {"city": "New York", "state": "NY",
+                          "funding": "Private, non-profit",
+                          "name": "Barnard College"},
+                         {"city": "New York", "state": "NY",
+                         "funding": "Public", "name": "Baruch College"},
+                         {"city": "New York", "state": "NY",
+                          "funding": "Private, non-profit",
+                          "name": "Fordham University"}]
+        # test to make sure this test was setup properly
 
         for idx, college_dict in enumerate(college_dicts):
-            college = College(college_dict["name"], college_dict["city"], college_dict["state"],college_dict["funding"])
-            
-        
+            college = College(college_dict["name"], college_dict["city"],
+                              college_dict["state"], college_dict["funding"])
+
             # assert there exists a "name" index in college dictionaries
             self.assertTrue(college.name == college_dicts[idx]['name'])
 
             # assert there are string values in the "name" index
             self.assertEquals(isinstance(college.name[3], str))
-        
-
-
 
 if __name__ == '__main__':
     unittest.main()
